545bd826319a6af9b65d8d4d629c43b0f85dfd20
Jerry Andersson <Kickupx@gmail.com>
2016-02-03 12:59:31 +0100

    Added funcional library.

---
+++ src/WaitingFor/RegexResults.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adminiator
+ * Date: 2016-02-03
+ * Time: 12:56
+ */
+
+namespace WaitingFor;
+
+
+class RegexResults
+{
+    private $regexes;
+
+    function __construct(array $regexes)
+    {
+
+    }
+}
