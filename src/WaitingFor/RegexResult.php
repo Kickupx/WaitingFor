822e51954e31d1c4ac5477c5355c12be6140bb89
Jerry Andersson <Kickupx@gmail.com>
2016-02-03 12:56:47 +0100

    Changed usage of RegexResult to contain one regex with priority attach to it.

--- src/WaitingFor/RegexResult.php
+++ src/WaitingFor/RegexResult.php
@@ -2,71 +2,59 @@
 namespace WaitingFor;
 
 class RegexResult {
-	private $regexMatches = [];
-	private $regexIndices = [];
-	private $regexMeta = [];
-	
-	function __construct(array $regexes, $str) {
-		$this->regexMeta = $regexes;
-	
-		foreach($regexes as $regex) {
-			$res = [];
-			preg_match_all($regex->regex(), $str, $res, PREG_OFFSET_CAPTURE);
-			array_push($this->regexMatches, $res[0]);
-			array_push($this->regexIndices, 0);
-		}
-	}
-	
-	function next() {
-		if(!count($this->regexMatches)) return false;
-		
-		$nextMatch = $this->findNext();
-		$delegate = $nextMatch["delegate"];
-		$delegate($nextMatch["match"]);
-		return true;
-	}
-	
-	function all() {
-		while($this->next()) {}
-	}
-	
-	private function findNext($matchesIndex = 0) {
-		//Current capture, from current regex index, and regex indice
-		$match = $this->regexMatches[$matchesIndex][$this->regexIndices[$matchesIndex]];
-		
-		//TODO: Get preg_match_all capture offset field
-		$startIndex = $match[1];
-		$endIndex = $startIndex + $match[0];
-		
-		//Check if there are any earlier matches and filter out overlapping
-		for($i = $matchesIndex + 1; $i < count($this->regexMatches); $i++) {
-			$lower_match = $this->regexMatches[$i][$this->regexIndices[$i]];
-			
-			//Check if is before
-			if($lower_match[1] < $startIndex) {
-			
-				//TODO: How to get actual hit in preg_match_all
-				//Check if overlapping
-				if(strlen($lower_match[0]) + $lower_match[1] < $startIndex)
-					return $this->findNext($i);
-			}
-			
-			//Increase match index for regex and remove if match string is empty
-			$matchLength = count($this->regexMatches[$matchesIndex]);
-			do {
-                $this->regexIndices[$i]++;
-                if (count($this->regexIndices[$i]) == $matchLength) {
-                    //TODO: Remove index $i from matches_indexes and $matches
-                }
-            } while($this->regexIndices[$i] <= $endIndex);
-		}
-		
-		//Increase matches index
-		$this->regexIndices[$matchesIndex]++;
-		
-		return [
-			"match" => $match,
-			"delegate" => $this->regexMeta[$matchesIndex]->delegate()
-		];
-	}
+    private $priority;
+	private $regex;
+    private $matches;
+    private $matchesCount;
+    private $current_index = 0;
+
+    /**
+     * RegexResult constructor.
+     * @param $regex
+     */
+    public function __construct($priority, $regex, $str)
+    {
+        $this->priority = $priority;
+        $this->regex = $regex;
+        preg_match_all($regex, $str, $this->matches, PREG_OFFSET_CAPTURE);
+        $this->matchesCount = count($this->matches);
+    }
+
+    function next() {
+        if($this->current_index == $this->matchesCount)
+            return null;
+        else {
+            $res = array_merge(
+                $this->matches[$this->current_index][0],
+                [
+                    'priority' => $this->priority
+                ]);
+            $this->current_index++;
+            return $res;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRegex()
+    {
+        return $this->regex;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCurrentIndex()
+    {
+        return $this->current_index;
+    }
 }
